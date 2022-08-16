9f4c07ed2828413527082aa4e408021482d77f1f
Tojaeung <kimwoon11@naver.com>
2022-08-17 00:21:55 +0900

    기본매핑 수정

--- src/main/java/jpashop/domain/Orderitem.java
+++ src/main/java/jpashop/domain/Orderitem.java
@@ -7,14 +7,15 @@ import javax.persistence.Id;
 
 @Entity
 public class Orderitem {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
     @Column(name = "ORDER_ID")
     private Long orderId;
 
-    @Column(name = "ORDER_ID")
+    @Column(name = "ITEM_ID")
     private Long itemId;
 
     private int orderPrice;
